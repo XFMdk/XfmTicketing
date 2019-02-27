523cbc345e02082a7f98da7bf24bc72632383f8a
algorythm <anders@wiberg.tech>
2019-02-27 17:23:10 +0100

    Setup configuration for Vue SPA

---
+++ resources/js/views/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>My Test App</h1>
+
+        <nav>
+            <router-link :to="{name: 'home'}">Home</router-link>
+            <router-link :to="{name: 'hello'}">Hello</router-link>
+        </nav>
+
+        <div class="container">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
