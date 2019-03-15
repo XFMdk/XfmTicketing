b71e119295588657f9846134260e23524f0bd696
algorythm <anders@wiberg.tech>
2019-03-15 10:58:19 +0100

    stuff

--- public/js/app.js
+++ public/js/app.js
@@ -15605,8 +15605,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/awo/development/itm/XfmTicketing/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/awo/development/itm/XfmTicketing/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/awo/development/itm/self-learning/XfmTicketing/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/awo/development/itm/self-learning/XfmTicketing/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
